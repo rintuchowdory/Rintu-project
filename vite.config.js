88eade1aaa25c7cdf4f76cf5ea2fdf6819d65900
Rintu-chowdory <chowdoryrintu492@gmail.com>
2026-01-18 18:17:21 +0100

    Transform into professional portfolio with enterprise design

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          icons: ['react-icons'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
 })
