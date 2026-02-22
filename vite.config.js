78d7eb63ac807bf3a1f67a78787e144d9b9827bf
Rintuuu <chowdoryrintu@yahoo.com>
2026-02-22 15:40:15 +0100

    your changes

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          icons: ['react-icons'],
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000,
-  },
-  server: {
-    port: 5173,
-    host: true
-  }
+  base: '/Rintu-project/',
 })
